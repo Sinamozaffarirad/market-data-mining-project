eba3a80ef0c9fc6fb6f5ca5f76322061a8a576ed
sinamozafarirad@gmail.com <sinamozafarirad@gmail.com>
2025-09-17 17:57:47 +0330

    Latest Main version before merge Samaneh 9/17

--- Website/market/catalog/admin.py
+++ Website/market/catalog/admin.py
@@ -3,16 +3,19 @@ from .models import Product, Order, OrderItem
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display  = ("sku", "name", "price")
+    list_display = ("sku", "name", "price")
     search_fields = ("sku", "name")
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display    = ("id", "customer", "created_at", "total")
-    list_filter     = ("created_at", "customer__user__username")
-    date_hierarchy  = "created_at"
+    list_display = ("id", "customer", "created_at", "total")
+    # The problematic filter has been removed.
+    list_filter = ("created_at",)
+    # A search field is added to allow searching by the customer's household key.
+    search_fields = ("customer__household_key",)
+    date_hierarchy = "created_at"
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display  = ("order", "product", "qty", "price", "subtotal")
+    list_display = ("order", "product", "qty", "price", "subtotal")
     search_fields = ("order__id", "product__name")
