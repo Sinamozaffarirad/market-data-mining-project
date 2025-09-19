828148a907a3fcc27b75a1ce1d4fd9fdd1fa1e10
Samaneh <s.tabandeh82@gmail.com>
2025-09-19 12:54:12 +0330

    Adding customer detail pages

--- Website/market/customers/urls.py
+++ Website/market/customers/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     # The 'name' has been changed from 'customer_search' to 'search'
     path('search/', views.customer_search, name='search'),
     path('detail/<int:pk>/', views.customer_detail, name='detail'),
+    path('detail/<int:pk>/recommendations/', views.customer_recommendations, name='recommendations'),
+    path('detail/<int:pk>/churn/', views.customer_churn, name='churn'),
+    path('detail/<int:pk>/purchases/', views.customer_purchases, name='purchases'),
 ]
